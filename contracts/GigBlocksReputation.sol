3ef3ff49a5acea8787d3db678e1c300eae94b570
wildanrhmn <80866806+wildanrhmn@users.noreply.github.com>
2024-08-23 20:40:39 +0700

    feat: add minting function & modifier for only gigblocks could call the contract

--- contracts/GigBlocksReputation.sol
+++ contracts/GigBlocksReputation.sol
@@ -9,7 +9,14 @@ import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 contract GigBlocksReputationTesting4 is ERC721, Ownable {
     using ECDSA for bytes32;
 
+    event ReputationMinted(address indexed user, uint256 tokenId);
+
     constructor() ERC721("GigBlocksReputation", "GBR") Ownable(msg.sender) {}
+
+    modifier onlyGigBlocksMain() {
+        require(msg.sender == gigBlocksMain, "Caller is not the GigBlocksMain contract");
+        _;
+    }
 
     address public gigBlocksMain;
 
@@ -21,4 +28,19 @@ contract GigBlocksReputationTesting4 is ERC721, Ownable {
 
     mapping(uint256 => ReputationMetadata) public reputationMetadata;
     mapping(address => uint256) public userToTokenId;
+
+    uint256 private _tokenIds;
+
+    function mintReputation(address user) external onlyGigBlocksMain {
+        require(userToTokenId[user] == 0, "User already has a reputation token");
+
+        _tokenIds++;
+        uint256 newTokenId = _tokenIds;
+
+        _safeMint(user, newTokenId);
+        reputationMetadata[newTokenId] = ReputationMetadata(0, false, 0);
+        userToTokenId[user] = newTokenId;
+
+        emit ReputationMinted(user, newTokenId);
+    }
 }
