0d96c08b497f6bcebb89acc69bdc33337c2d27b1
wildanrhmn <80866806+wildanrhmn@users.noreply.github.com>
2024-08-23 18:59:46 +0700

    feat: GigBlocksBase inheritance init

--- contracts/GigBlocksBase.sol
+++ contracts/GigBlocksBase.sol
@@ -1,2 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IGigBlocks.sol";
+import "./GigBlocksUserManager.sol";
+import "./GigBlocksEnums.sol";
+
+abstract contract GigBlocksBase is Ownable, GigBlocksUserManager, IGigBlocks {}
